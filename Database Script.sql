15bd2065e69f784a94bd46149b88af77136b58ba
Ankush Agnihotri <ankush1802@outlook.com>
2024-04-28 14:22:34 +0530

    Database script

---
+++ Database Script.sql
@@ -0,0 +1,147 @@
+USE [master]
+GO
+/****** Object:  Database [EmployeManagementGraphQL]    Script Date: 04/28/24 14:21:36 ******/
+CREATE DATABASE [EmployeManagementGraphQL]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'EmployeManagementGraphQL', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\EmployeManagementGraphQL.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'EmployeManagementGraphQL_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\EmployeManagementGraphQL_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [EmployeManagementGraphQL].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET  MULTI_USER 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [EmployeManagementGraphQL]
+GO
+/****** Object:  Table [dbo].[Employee]    Script Date: 04/28/24 14:21:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Employee](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](1024) NOT NULL,
+	[LastName] [nvarchar](1024) NOT NULL,
+	[Email] [nvarchar](1024) NOT NULL,
+ CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Review]    Script Date: 04/28/24 14:21:36 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Review](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Rate] [int] NOT NULL,
+	[Comment] [nvarchar](max) NOT NULL,
+	[EmployeeId] [int] NOT NULL,
+ CONSTRAINT [PK_Review] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Employee] ON 
+GO
+INSERT [dbo].[Employee] ([Id], [FirstName], [LastName], [Email]) VALUES (1, N'Ankush', N'Agnihotri', N'ankush1802@outlook.com')
+GO
+INSERT [dbo].[Employee] ([Id], [FirstName], [LastName], [Email]) VALUES (2, N'Amit', N'Agnihotri', N'amit.agnihotri@gmail.com')
+GO
+INSERT [dbo].[Employee] ([Id], [FirstName], [LastName], [Email]) VALUES (12, N'Rahul', N'Agnihotri', N'rahul.agnihotri@gmail.com')
+GO
+SET IDENTITY_INSERT [dbo].[Employee] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Review] ON 
+GO
+INSERT [dbo].[Review] ([Id], [Rate], [Comment], [EmployeeId]) VALUES (1, 5, N'Test Comment 1', 1)
+GO
+INSERT [dbo].[Review] ([Id], [Rate], [Comment], [EmployeeId]) VALUES (2, 5, N'Test Comment 2', 1)
+GO
+INSERT [dbo].[Review] ([Id], [Rate], [Comment], [EmployeeId]) VALUES (3, 4, N'Test Comment 3', 2)
+GO
+INSERT [dbo].[Review] ([Id], [Rate], [Comment], [EmployeeId]) VALUES (15, 5, N'This is Comment 4', 12)
+GO
+SET IDENTITY_INSERT [dbo].[Review] OFF
+GO
+ALTER TABLE [dbo].[Review]  WITH CHECK ADD  CONSTRAINT [FK_Review_Employee] FOREIGN KEY([EmployeeId])
+REFERENCES [dbo].[Employee] ([Id])
+GO
+ALTER TABLE [dbo].[Review] CHECK CONSTRAINT [FK_Review_Employee]
+GO
+USE [master]
+GO
+ALTER DATABASE [EmployeManagementGraphQL] SET  READ_WRITE 
+GO
